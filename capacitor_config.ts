02f44ca13a118ecc3edbff851c7ee6517b01976f
Zeuva <zeuvastec@gmail.com>
2026-08-20 18:53:14 -0300

    Add files via upload

---
+++ capacitor_config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zeuvastec.verbixse',
+  appName: 'VERBIXSE ZEUVASTEC',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
